8ae4aec6b6abb90ca5393d9596560c04a9820895
Michelle <michelle.van.kuijk001@gmail.com>
2022-09-06 16:14:33 +0200

    Create playRound function to compare playerSelection with computerSelection and return win or lose result

--- script.js
+++ script.js
@@ -10,4 +10,29 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+  // write logic with if else statement comparing the computer's selection with player selection to see who has won.
+  if(playerSelection === computerSelection) {
+    return "It's a Tie!"
+  } else if ((playerSelection === "rock") && (computerSelection === "scissors")){
+    return "You Won! Rock beats Scissors";
+  } else if ((playerSelection === "rock") && (computerSelection === "paper")) {
+    return "You Lose! Paper beats Rock";
+  } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+    return "You Won! Scissors beats Paper";
+  } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+    return "You Lose! Rock beats Scissors";
+  } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+    return "You Lose! Scissors beats Paper";
+  } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+    return "You Won! Paper beats Rock";
+  } else {
+    return "Please enter a valid option";
+  }
+
+}
+
+const playerSelection = prompt("Choose rock, paper or scissors", "rock").toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
