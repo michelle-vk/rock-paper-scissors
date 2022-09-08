b79cd9098313684b69408bc2fc755a06eca41d9c
Michelle <michelle.van.kuijk001@gmail.com>
2022-09-08 13:10:32 +0200

    Create game function that keeps track of the score and a function that reports a winner

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   // write logic with if else statement comparing the computer's selection with player selection to see who has won.
-  if(playerSelection === computerSelection) {
+  if (playerSelection === computerSelection) {
     return "It's a Tie!"
   } else if ((playerSelection === "rock") && (computerSelection === "scissors")){
     return "You Won! Rock beats Scissors";
@@ -32,7 +32,45 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = prompt("Choose rock, paper or scissors", "rock").toLowerCase();
-const computerSelection = getComputerChoice();
+function game() {
+  // const playerSelection = prompt("Choose rock, paper or scissors", "rock").toLowerCase();
+  const computerSelection = getComputerChoice();
+  // keep track of the results of each round to be able to declare a winner
+  let playerResult = 0;
+  let computerResult = 0;
+  for(let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Choose rock, paper or scissors", "rock").toLowerCase();
+    let play = playRound(playerSelection, computerSelection);
+    if (play.includes("Won")) {
+      playerResult += 1;
+    } else if (play.includes("Lose")) {
+      computerResult += 1;
+    } else if (play.includes("Tie")){
+      playerResult += 0;
+      computerResult += 0;
+    } else {
+      console.log("Non valid");
+    }
+  }
 
-console.log(playRound(playerSelection, computerSelection));
+  const results = `Score player: ${playerResult} - Score computer: ${computerResult}`;
+  console.log(results);
+  reportWinner(playerResult, computerResult);
+}
+
+function reportWinner(player, computer) {
+  // this function the results of the game to declare a winner
+  if (player > computer) {
+    console.log("CONGRATS! You won the GAME!");
+  } else if (player < computer) {
+    console.log("I'm sorry, but you lost. The computer won the game.");
+  } else {
+    console.log("It's a tie! Nobody won or lost");
+  }
+}
+
+game()
+
+
+
+
